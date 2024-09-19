2e3e43777abea5b1bf4f59c701658ff9ded7966f
Naelson-exe <fubaranelson84@gmail.com.norepyly.github.com>
2024-09-19 15:09:32 +0100

    Update: Added the 16 * 16 grid using only JS

---
+++ script.js
@@ -0,0 +1,13 @@
+const container = document.querySelector('.container');
+
+function random(value){
+    return Math.floor(Math.random() * value);
+}
+
+for(let i = 0; i < 16 *  16; i++){
+    const cell = document.createElement('div');
+    cell.classList.add('container-cell');
+    cell.style.backgroundColor = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+
+    container.appendChild(cell);
+}
